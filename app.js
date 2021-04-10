8319935ce11ebb22af66539829b74844a4b052f9
SenciZ <zenkovic88@gmail.com>
2021-04-10 17:14:08 -0500

    Commit for fun before scratching all of the JS and resetarting

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
-function add(a,b){
-    return a+b;
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearDisplay);
+
+function clearDisplay(){
+    display.textContent ="0";
+    numStore.number1 = 0;
+    numStore.number2 = 0;
+    numStore.operator = null;
+
 }
 
+const display = document.querySelector(".display");
+
+function add(a,b){
+    numStore.operator = add;
+    display.textContent = parseInt(a) + parseInt(b);
+}
+
+let numStore={
+    number1: 0,
+    number2: 0,
+    operator: null
+};
+
 function subtract(a,b){
-    return a-b;
+    display.textContent = parseInt(a) - parseInt(b);
 }
 
 function multiply(a,b){
-    return a*b;
+    display.textContent = parseInt(a) * parseInt(b);
 }
 
 function divide(a,b){
-    return a/b;
+    display.textContent = parseInt(a) / parseInt(b);
+
 }
 
-function operate(operator, num1, num2){
-    operator(num1, num2);
+function operate(num1, operator, num2){
+    return operator(num1, num2);
 }
 
-const clearBtn = document.querySelector(".clear");
-clearBtn.addEventListener("click", clearResult);
+const digitPressed = document.querySelectorAll(".digitBtn");
 
-function clearResult(){
-    result.textContent ="";
-}
+digitPressed.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+       if(numStore.operator == null){
+        numStore.number1 = numStore.number1 += e.target.value;
+       } else if(numStore.number2 == 0){
+        numStore.number2 = numStore.number2 += e.target.value;
+       }
+    });
+ });
+
+ const addBtn = document.querySelector(".addBtn");
+
+addBtn.addEventListener("click", function(e){
+    numStore.operator = e.target.value;
+})
 
 
+const equalButton = document.querySelector(".equalsBtn");
+equalButton.addEventListener("click", function(){
+    numStore.operator(numStore.number1, numStore.number2);
+    numStore.number1 = 0;
+    numStore.number2 = 0;
+    numStore.operator = null;
+});
 
-const result = document.querySelector(".result");
