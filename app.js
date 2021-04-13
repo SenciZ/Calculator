46a1feb218bb0309b8a9d43f1c1e3edc8b57625a
SenciZ <zenkovic88@gmail.com>
2021-04-12 19:37:19 -0500

    Work on getting the calculations working.

--- app.js
+++ app.js
@@ -1,68 +1,91 @@
+let operatorStorage = null;
+let firstDigit = 0;
+let secondDigit = 0;
+
+const display = document.querySelector(".display");
+display.textContent = firstDigit;
+
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", clearDisplay);
 
 function clearDisplay(){
-    display.textContent ="0";
-    numStore.number1 = 0;
-    numStore.number2 = 0;
-    numStore.operator = null;
+    firstDigit = 0;
+    secondDigit = 0;
+    operatorStorage = null;
+    display.textContent = firstDigit;
 
 }
-
-const display = document.querySelector(".display");
 
 function add(a,b){
-    numStore.operator = add;
-    display.textContent = parseInt(a) + parseInt(b);
+    return parseInt(a) + parseInt(b);
 }
 
-let numStore={
-    number1: 0,
-    number2: 0,
-    operator: null
-};
-
 function subtract(a,b){
-    display.textContent = parseInt(a) - parseInt(b);
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a,b){
-    display.textContent = parseInt(a) * parseInt(b);
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a,b){
-    display.textContent = parseInt(a) / parseInt(b);
-
-}
-
-function operate(num1, operator, num2){
-    return operator(num1, num2);
+    return parseInt(a) / parseInt(b);
 }
 
 const digitPressed = document.querySelectorAll(".digitBtn");
-
 digitPressed.forEach(btn => {
     btn.addEventListener('click', (e) => {
-       if(numStore.operator == null){
-        numStore.number1 = numStore.number1 += e.target.value;
-       } else if(numStore.number2 == 0){
-        numStore.number2 = numStore.number2 += e.target.value;
+       if(firstDigit == 0 | operatorStorage == null){
+        firstDigit += parseInt(e.target.value);
+        display.textContent += parseInt(e.target.value);
+       } else if(operatorStorage != null && firstDigit != 0){
+        secondDigit += parseInt(e.target.value);
+        display.textContent += parseInt(e.target.value);
        }
     });
  });
 
- const addBtn = document.querySelector(".addBtn");
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", function(){
+    operatorStorage = add;
+    display.textContent = `${firstDigit} + `
+})
 
-addBtn.addEventListener("click", function(e){
-    numStore.operator = e.target.value;
+const subtractBtn = document.querySelector(".subtractBtn");
+subtractBtn.addEventListener("click", function(){
+    operatorStorage = subtract;
+    display.textContent = `${firstDigit} - `
+})
+
+const multiBtn = document.querySelector(".multiBtn");
+multiBtn.addEventListener("click", function(){
+    operatorStorage = multiply;
+    display.textContent = `${firstDigit} * `
+})
+
+const divideBtn = document.querySelector(".divideBtn");
+divideBtn.addEventListener("click", function(){
+    operatorStorage = divide;
+    display.textContent = `${firstDigit} / `
 })
 
 
+function operate(num1, operator, num2){
+    display.textContent = operator(num1, num2);
+}
+
 const equalButton = document.querySelector(".equalsBtn");
 equalButton.addEventListener("click", function(){
-    numStore.operator(numStore.number1, numStore.number2);
-    numStore.number1 = 0;
-    numStore.number2 = 0;
-    numStore.operator = null;
+    if(firstDigit == 0){
+        return;
+    }
+    operate(firstDigit, operatorStorage, secondDigit);
+    firstDigit = parseInt(display.textContent);
+    secondDigit = 0;
 });
+
+
+
+
+
 
